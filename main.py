3f12381e5f1ff646a03f6e873ed96cebc0e6d542
IsaiahSama <zeldaplayergl@gmail.com>
2021-01-05 18:40:37 -0400

    Added in functionality for reading schedules

--- main.py
+++ main.py
@@ -4,12 +4,12 @@
 # structure of dict:
 # {
 #     username :username,
-#     daily: {
+#     Daily: {
 #             schedulename: {
 #                 times: task
 #             }
 #         },
-#     weekly: {
+#     Weekly: {
 #         schedulename: {
 #             day: {
 #                 times: task
@@ -61,6 +61,10 @@ def verifyResponse(prompt):
         if confirmation == 1: return response
         else: clrs()
 
+def save(mydict):
+    with open("C:\\ScheduleKeeper\\schedule.json", "w") as f:
+        json.dump(mydict, f, indent=4)
+
 class Create:
     def __init__(self, name, mydict):
         self.name = name
@@ -100,6 +104,68 @@ class Create:
             prompt = "Would you like to set another task and time?\n\n1)Yes\n\n2)No"
             answer = verifyNumber(prompt, [1,2])
             if answer == 2: break
+        save(mydict)
+
+class Read:
+    def __init__(self, schedule):
+        self.schedule = schedule
+
+    def view_weekly(self):
+        while True:
+            mydict = self.schedule["Weekly"]
+            clrs("Showing all of the names of your weekly schedules")
+            keys = [key for key in mydict.keys()]
+            for position, schedulename in enumerate(mydict.keys()):
+                print(f"{position}) {schedulename}\n")
+        
+            print("\nEnter the name of the schedule you would like to view, or enter 0 to return to menu")
+            answer = input(": ")
+            if answer == "0": print("Returning to main menu"); return
+            if answer.capitalize() not in keys: print("You don't seem to have a schedule with that name")
+            else: 
+                try:
+                    to_send = mydict[answer]
+                except:
+                    clrs(f"Something went wrong getting the schedule {answer}")
+                    continue
+                clrs(f"Showing your schedule with the name {answer}")
+                self.show_data(to_send)
+
+    def view_daily(self):
+        while True:
+            mydict = self.schedule["Daily"]
+            clrs("Showing all of the names of your daily schedules")
+            keys = [key for key in mydict.keys()]
+            for position, schedulename in enumerate(mydict.keys()):
+                print(f"{position}) {schedulename}\n")
+        
+            print("\nEnter the name of the schedule you would like to view, or enter 0 to return to menu")
+            answer = input(": ")
+            if answer == "0": print("Returning to main menu"); return
+            if answer.capitalize() not in keys: print("You don't seem to have a schedule with that name")
+            else: 
+                try:
+                    to_send = mydict[answer]
+                except:
+                    clrs(f"Something went wrong getting the schedule {answer}")
+                    continue
+                clrs(f"Showing your schedule with the name {answer}")
+                self.show_data(to_send)
+
+    def show_data(self, data):
+        with open("tempfile.json", "w") as f:
+            dump(data, f, indent=4)
+
+        print("Opening data in your notepad. Return to me and press enter to proceed")
+        sleep(2)
+        system("tempfile.json")
+        input(": ")
+        try:
+            remove("tempfile.json")
+        except FileNotFoundError:
+            print("It would seem as though you had already removed the file")
+        clrs()
+        
 
 class Main:
     def __init__(self):
@@ -127,10 +193,11 @@ class Main:
 
     def menu(self):
         clrs(f"How May I help you today {self.schedule['username']}?\n\n")
-        prompt = "1)Create A New Schedule\n\n2)View an Existing Schedule\n\n3)Update an Existing Schedule\n\n4)Delete an Existing Schedule\n\n5)Exit this program\n\n6)Uninstall this program"
+        prompt = "1)Create A New Schedule\n\n2)View an Existing Schedule\n\n3)Update an Existing Schedule\n\n4)Delete an Existing Schedule\n\n5)Track a Schedule\n\n6)Exit this program\n\n7)Uninstall this program"
         response = verifyNumber(prompt, [1,2,3,4,5,6])
-        if response == 6: self.uninstall()
-        if response == 5: self.close()
+        if response == 7: self.uninstall()
+        if response == 6: self.close()
+        if response == 5: self.track()
         if response == 4: self.delete()
         if response == 3: self.update()
         if response == 2: self.view()
@@ -146,13 +213,20 @@ class Main:
             prompt = "\n1)Create day-to-day schedule\n\n2)Create weekly schedule\n\n3)Return to menu"
             answer = verifyNumber(prompt, [1,2,3])
             if answer == 3: return
-            create = Create(name, self.schedule)
+            create = Create(name.capitalize(), self.schedule)
             if answer == 2: create.create_weekly()
             if answer == 1: create.create_daily()
 
 
     def view(self):
-        if not self.schedule: print("You have no existing schedules to view."); return    
+        if not self.schedule: print("You have no existing schedules to view."); return
+        clrs("Would you like to view a daily or weekly schedule?")
+        prompt = "\n1)Daily\n2)Weekly\n3)Return to menu"
+        response = verifyNumber(prompt, [1,2,3])
+        if response == 3: print("Returning to menu"); return
+        read = Read(self.schedule)
+        if response == 2: read.view_weekly()
+        if response == 1: read.view_daily()
 
 
     def update(self):
@@ -161,6 +235,9 @@ class Main:
 
     def delete(self):
         if not self.schedule: print("You have no existing schedules to delete."); return    
+
+    def track(self):
+        if not self.schedule: print("You have no existing schedules to track."); return    
 
 
     def close(self):
